eb9f281276735786dcb21a77e350db497ec62519
Debottam Kar <debottamkar2003@gmail.com>
2025-06-14 20:02:22 +0530

    ejs master class

---
+++ ejs_masterclass/server.js
@@ -0,0 +1,52 @@
+import { fileURLToPath } from "url";
+import express from 'express';
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const users = [
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 25 },
+    { name: "Charlie", age: 28 }
+];
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+console.log(__dirname);
+console.log(__filename);
+
+app.get('/home', (req, res) => {
+    res.render('home', { data: [{ name: "debottam kar", age: 22 }] });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const { name, age } = req.body;
+    console.log(name, age);
+    res.redirect('/landing');
+});
+
+app.get('/landing', (req, res) => {
+    res.render('landing');
+});
+
+app.get('/landingPageData', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(users);
+    }, 5000);
+});
+
+app.listen(3333, () => {
+    console.log("Server is running on port 3333");
+});
+
